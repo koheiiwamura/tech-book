13be2a596812bb6e78c98e412055365e8f34e77c
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-09-21 13:50:24 +0900

    git-clone-all

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,23 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require js/jquery-1.11.3.min.js
+//= require js/jquery-migrate-1.2.1.min.js
+//= require bootstrap/js/bootstrap.js
+//= require js/jquery.waypoints.min.js
+//= require js/jquery.easing.1.3.js
+//= require js/SmoothScroll.min.js
+//= require js/jquery.slicknav.min.js
+//= require js/jquery.placeholder.min.js
+//= require js/instagram.min.js
+//= require js/spin.min.js
+//= require js/jquery.introLoader.min.js
+//= require js/select2.full.js
+//= require js/jquery.responsivegrid.js
+//= require js/ion.rangeSlider.min.js
+//= require js/readmore.min.js
+//= require js/slick.min.js
+//= require js/validator.min.js
+//= require js/jquery.raty.js
+//= require js/customs.js
+
