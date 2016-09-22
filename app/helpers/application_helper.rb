68d5c1133b094715377f8b29064d2780b784bdba
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-09-22 21:03:45 +0900

    devise-user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def resource_name
+     :user
+  end
+
+  def resource
+     @resource ||= User.new
+  end
+
+  def devise_mapping
+     @devise_mapping ||= Devise.mappings[:user]
+  end
+
+#   def after_sign_in_path_for(resource)
+#   session[:previous_url] || root_path
+# end
 end
