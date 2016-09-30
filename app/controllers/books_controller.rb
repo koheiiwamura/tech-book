36268d875f313847f741c82f9765db522ff19597
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-09-30 11:12:29 +0900

    出品した本のedit、destroy実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   def index
     @books = Book.all.includes(:user).page(params[:page]).per(8)
@@ -20,12 +21,38 @@ class BooksController < ApplicationController
     end
   end
 
+  def show
+  end
+
   def edit
+  end
+
+  def update
+    if @book.update(update_params)
+      redirect_to root_path, notice:"更新しました"
+    else
+      flash[:alert] = "更新できませんでした"
+      render :edit
+    end
+  end
+  def destroy
+    if current_user.id == @book.user_id
+      @book.destroy
+      redirect_to root_path, notice:"削除しました"
+    else
+      redirect_to root_path, alert:"削除できませんでした"
+    end
   end
 
   private
   def create_params
     params.require(:book).permit(:image,:title,:content,:state,:price,:postage).merge(user_id:current_user.id, tag_list: params[:book][:tag])
+  end
+  def update_params
+    params.require(:book).permit(:image,:title,:content,:state,:price,:postage).merge(user_id:current_user.id, tag_list: params[:book][:tag])
+  end
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
 
