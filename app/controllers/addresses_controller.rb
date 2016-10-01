4c552af19bfba5977448f00216e3828af8f78e1f
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-10-01 16:17:24 +0900

    fix-view-because::generate-addresses_table

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,16 @@
 class AddressesController < ApplicationController
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(update_params)
+      redirect_to :controller => 'users', :action => 'show', notice:"更新しました"
+    else
+      flash[:alert] = "更新できませんでした"
+      render "user/edit"
+    end
+  end
+  private
+  def update_params
+    params.require(:address).permit(:last_name, :first_name, :post_number, :street_address, :phone_number)
+  end
 end
