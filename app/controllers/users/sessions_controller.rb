0688ae31a006a71c3ccda9c92c3acdfabccb8feb
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-10-14 20:36:53 +0900

    user-edit

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   def create
+    devise_parameter_sanitizer.permit([:sign_up, :sign_in], keys: [:username])
     resource = warden.authenticate!(auth_options)
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
