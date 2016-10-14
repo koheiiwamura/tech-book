5485ec1fe139f52727352fe456c6c3191853839d
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-10-14 20:36:38 +0900

    delete-unnessesary-code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,22 +12,10 @@ class UsersController < ApplicationController
     @bank = Bank.where(user_id: @user.id).first_or_create
     @books = @user.books_of_buyer.page(params[:page]).per(8).order("id DESC")
   end
-  def update
-    if @user.update(update_params)
-      redirect_to root_path,notice:"更新に成功しました"
-    else
-      flash.now[:alert] = "更新に失敗しました"
-      render :edit
-    end
-  end
 
   private
   def set_user
     @user = User.find(params[:id])
-  end
-
-  def update_params
-    params.require(:user).permit(:username, :email, :password, :first_name, :last_name, :postal_code, :street_adress, :phone_number, :avatar)
   end
 
   def check_logined
