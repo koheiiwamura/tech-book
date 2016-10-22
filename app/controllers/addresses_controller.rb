1ed2473c83f27e5893c80c766a5ca3d6d06b1cc7
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-10-22 20:39:29 +0900

    no message

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,18 +1,6 @@
 class AddressesController < ApplicationController
-  skip_before_filter :verify_authenticity_token ,:only=>[:update]
-
-  def create
-    @address = Address.new(update_params)
-    if @address.save
-      redirect_to ({:controller => 'users', :action => 'show', id: @address.user}), notice:"更新しました"
-    else
-      flash[:alert] = "更新できませんでした"
-      render "user/edit"
-    end
-  end
 
   def update
-    binding.pry
     @address = Address.find(params[:id])
     if @address.update(update_params)
       redirect_to ({:controller => 'users', :action => 'show', id: @address.user}), notice:"更新しました"
