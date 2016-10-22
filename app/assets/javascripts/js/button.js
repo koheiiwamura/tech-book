636bfa3b12175e57a80531039cca2fad7cc19266
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-10-22 12:22:01 +0900

    packaging button.js

--- app/assets/javascripts/js/button.js
+++ app/assets/javascripts/js/button.js
@@ -3,9 +3,22 @@ $(function() {
 
   $('#accept_booking').click(function() {
     if ($(this).prop('checked') == false) {
-      $('#submit').attr('disabled', 'disabled');
+      $('#submit').attr('disabled', 'disabled').removeClass('btn-primary');
     } else {
-      $('#submit').removeAttr('disabled');
+      $('#submit').removeAttr('disabled').addClass('btn-primary');
+    }
+  });
+});
+
+$(function() {
+  if ($('#comment_text').val().replace(/[\n\s　]/g, "").length == 0) {
+    $('#submit').attr('disabled', 'disabled');
+  }
+  $('#comment_text').bind('keydown keyup keypress change', function() {
+    if ($(this).val().replace(/[\n\s　]/g, "").length > 0) {
+      $('#submit').removeAttr('disabled').addClass('btn-primary');
+    } else {
+      $('#submit').attr('disabled', 'disabled').removeClass('btn-primary');
     }
   });
 });
