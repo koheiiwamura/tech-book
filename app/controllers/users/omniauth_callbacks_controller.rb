4c9c00e831160e1955035a8123cb25289f81d118
koheiiwamura <hershey-s-.7s2.w1@docomo.ne.jp>
2016-11-03 20:41:49 +0900

    Revert "create Facebook password"

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,7 +11,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = "facebookから承認されました。新規登録の場合、送信メールのテキストにあるURLから、パスワードを設定してください"
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
